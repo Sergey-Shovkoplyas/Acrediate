6f934bbe0e84fcaf5640f4d3f066de659fe938d3
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-05-26 23:01:49 +0300

    Fix select

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,12 @@
-window.onload = function () {
-	
-	console.log('js is working!!!'); 
 
-}
+$( document ).ready(function() {
 
-/* $( document ).ready(function() {
+	$('select.registration__item').styler();
+	$('.jq-selectbox__select').on('click', function(){
+		$(this).toggleClass('active');
+	});
+	$('.jq-selectbox__dropdown').on('click', function(){
+		$(this).prev('.jq-selectbox__select').toggleClass('active');
+	});
 
-	console.log( 'JQuery!!!' );
-
-}); */
+});
